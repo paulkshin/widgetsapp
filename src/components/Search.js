029d0953b988680f9388aee469378de28d77d073
Paul Shin <paulkisooshin@gmail.com>
2021-11-03 12:04:33 -0700

    axios search request added

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,12 +1,23 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Search = () => {
   const [term, setTerm] = useState("");
 
-  console.log("I RUN WITH EVERY RENDER");
-
   useEffect(() => {
-    console.log("I RUN AFTER EVERY RENDER AND AT INITIAL RENDER");
+    const search = async () => {
+      await axios.get("https://en.wikipedia.org/w/api.php", {
+        params: {
+          action: "query",
+          list: "search",
+          origin: "*",
+          format: "json",
+          srsearch: term,
+        },
+      });
+    };
+
+    search();
   }, [term]);
 
   return (
